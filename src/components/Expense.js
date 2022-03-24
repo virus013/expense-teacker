e46e4249fb2504af0a3507dc05d5d06ff558ce6f
virus8455 <prassanajoshi8455@gmail.com>
2022-03-24 12:47:49 +0530

    My project

---
+++ src/components/Expense.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Expense({ income, expense }) {
+    return (
+        <div>
+            <h2>Your Balance</h2>
+            <div className='balance-val'>INR{income - expense}</div>
+            <div className='row row-expense'>
+                <div className='col col-income'>
+                    <span>
+                        <h3>Income</h3>
+                        <div className='income-text'>INR{income}</div>
+                    </span>
+                </div>
+                <div className='col col-expense'>
+                    <h3>Expense</h3>
+                    <div className='expense-text'>INR{expense}</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Expense;
